970b4eb011f32c5b7923fc62978dcd62102b5a3a
Jonathan Linat <jonathan.linat@gmail.com>
2018-12-01 12:51:15 -0300

    Deleted unnecessary 'babel-plugin-transform-class-properties' package.

--- src/modules/js/log.js
+++ src/modules/js/log.js
@@ -1,7 +1,7 @@
-/*
+/**
  * MIT License
  *
- * Copyright (c) 2018 Jonathan Linat <https://www.github.com/jonathanlinat>
+ * Copyright (c) 2018 Jonathan Linat <https://github.com/jonathanlinat>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
