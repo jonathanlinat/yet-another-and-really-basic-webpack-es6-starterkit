cf1c01d43cedf83a716788bad3e63c5beb692b5c
Jonathan Linat <jonathan.linat@gmail.com>
2019-08-09 21:55:33 -0400

    Updated NPM packages to their latest available version.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const TerserPlugin = require('terser-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const webpack = require('webpack')
 
 module.exports = (env, options) => {
